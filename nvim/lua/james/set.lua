e1e47eed2d36ff7689d9a7900908e99161f07a22
James Black <31362189+LilMikey-CN@users.noreply.github.com>
2022-10-03 01:26:50 +0800

    no wrap this time, try try

--- nvim/lua/james/set.lua
+++ nvim/lua/james/set.lua
@@ -23,7 +23,7 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.wrap = true -- Also different
+vim.opt.wrap = false -- Also different
 
 vim.g.mapleader = " " -- Set <leader> key
 
