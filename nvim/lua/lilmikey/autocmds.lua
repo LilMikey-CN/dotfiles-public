f59b318ff13d308482e7f69dee79da6867440361
Xingkai Jiao (James) <31362189+LilMikey-CN@users.noreply.github.com>
2024-12-04 15:22:24 +1100

    add jsx and tsx file type to the autogroup

--- nvim/lua/lilmikey/autocmds.lua
+++ nvim/lua/lilmikey/autocmds.lua
@@ -13,7 +13,7 @@ augroup('setIndent', { clear = true })
 autocmd('Filetype', {
   group = 'setIndent',
   pattern = { 'xml', 'html', 'xhtml', 'css', 'scss', 'javascript', 'typescript',
-    'yaml', 'lua'
+    'yaml', 'lua', 'javascriptreact', 'typescriptreact'
   },
   command = 'setlocal shiftwidth=2 tabstop=2'
 })
