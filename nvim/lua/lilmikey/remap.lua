1f951dbdd8e1fc5af3bcb4d915ae3054879c273f
Xingkai Jiao (James) <31362189+LilMikey-CN@users.noreply.github.com>
2024-12-04 15:23:37 +1100

    failed to fix the weird behavior of <Tab> with harpoon... So commented it

--- nvim/lua/lilmikey/remap.lua
+++ nvim/lua/lilmikey/remap.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- vim.keymap.set("n", "<Tab>", "<Nop>")
+
 -- vim-tmux-navigator
 vim.keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>")
 vim.keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>")
