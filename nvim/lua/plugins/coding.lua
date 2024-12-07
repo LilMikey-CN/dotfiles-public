a99dc15482773361a78fc3431c978ca6a3dbdcce
Xingkai Jiao (James) <31362189+LilMikey-CN@users.noreply.github.com>
2024-12-07 11:16:19 +1100

    added lsp and ui setups

---
+++ nvim/lua/plugins/coding.lua
@@ -0,0 +1,8 @@
+return {
+  -- Incremental rename <leader>cr
+  {
+    "smjonas/inc-rename.nvim",
+    cmd = "IncRename",
+    config = true,
+  },
+}
---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,55 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+
+      -- set popup border
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+
+  -- Change the notification timeout
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 5000,
+    },
+  },
+
+  -- statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      local LazyVim = require("lazyvim.util")
+      opts.sections.lualine_c[4] = {
+        LazyVim.lualine.pretty_path({
+          length = 0,
+          relative = "cwd",
+          modified_hl = "MatchParen",
+          directory_hl = "",
+          filename_hl = "Bold",
+          modified_sign = "",
+          readonly_icon = " 󰌾 ",
+        }),
+      }
+    end,
+  },
+
+  -- animations
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = false,
+      }
+    end,
+  },
+}
